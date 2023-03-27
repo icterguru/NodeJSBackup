3d2befb8f2c45534a6021ec9cbdf36125a94a12f
ICTER Guru <ictergiru@gmail.com>
2023-03-27 14:56:49 -0400

    Hello Friend program added

---
+++ HelloFriendIFFY.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+(
+    function f1() {
+
+        let name = prompt("Please ener your name and press Enter:");
+        console.log(`Hello ... ${name}! \nHow are you doing today?`);
+    }
+)();
